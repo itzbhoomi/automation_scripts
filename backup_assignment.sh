5af716112384a4047e4b976f03138a754cc53a36
itzbhoomi <bhoomisakhrani25@gmail.com>
2025-03-26 11:27:47 +0530

    readme changed

---
+++ backup_assignment.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+###########
+
+# Date: 26 March 2025
+# A bash script that automates file backups, restoration, and cleanup, managing each feature in separate branches.
+
+###########
+
+g
