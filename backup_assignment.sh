87fbdcf2bb4b32537bae5caae79b55b5bda241ff
itzbhoomi <bhoomisakhrani25@gmail.com>
2025-03-26 16:11:39 +0530

    main

--- backup_assignment.sh
+++ backup_assignment.sh
@@ -19,3 +19,29 @@ filename=backup_$timestamp.tar.gz
 tar -czvf "$dest/$filename" "$backup"
 echo "File backup completed: $dest/$filename"
 }
+
+############
+# Restore
+############
+
+restore(){
+set -e
+mkdir -p "$HOME/restore"
+ls /home/$USER/.backup/
+echo "Enter the filename which you wish to restore: "
+read filename
+
+tar -xvf "$HOME/.backup/$filename" -C "$HOME/restore/"
+echo "File restored: $filename"
+}
+
+###########
+# Delete
+###########
+
+delete(){
+set -e
+TARGET_DIR="/home/$USER/.backup/"
+find "$TARGET_DIR" -cmin +1 -delete
+echo "Deletion completed!"
+}
