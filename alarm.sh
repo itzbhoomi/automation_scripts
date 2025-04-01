6d3353750d8c17a4da5516c18aecb981fb5e3223
itzbhoomi <bhoomisakhrani25@gmail.com>
2025-04-02 02:04:30 +0530

    liveclock

---
+++ alarm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+###########
+
+# Date: 26 March 2025
+# Author: Bhoomi 
+# A bash script for alarm clock
+
+###########
+
+#LIVE_CLOCK
+
+###########
+
+while true 
+    do 
+        clear
+        echo (Date +T%)
+        sleep
+    done
